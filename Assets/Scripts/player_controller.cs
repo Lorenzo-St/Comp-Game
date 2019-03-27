33a838eaba2abeb5879eaf1a73d883d265632d3f
Lorenzo-St <chd_frost@yahoo.com>
2019-03-27 08:45:16 -0700

    add da stuff

---
+++ Assets/Scripts/player_controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+    private player_motor PM;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PM = GetComponent<player_motor>();
+    }
+
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        PerformMove();
+    }
+
+    public void PerformMove()
+    {
+        
+        
+    }
+}
---
+++ Assets/Scripts/player_motor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_motor : MonoBehaviour
+{
+
+    private player_controller PC;
+    
+    public float speed = 5f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PC = GetComponent<player_controller>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
