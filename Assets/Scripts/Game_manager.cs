40c667bff715446bee1481bdacafcce280259b97
Lorenzo-St <chd_frost@yahoo.com>
2019-04-12 08:51:32 -0700

    stuff

---
+++ Assets/Scripts/Game_manager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game_manager : MonoBehaviour
+{
+
+    // GameObjects
+    public GameObject Player;
+    
+    // Data Holders
+    public int score;
+    public float Time_left = 60f;
+    public int min = 4;
+    
+    // GUI Stuff
+    [SerializeField]
+    private Text Score_Display;
+
+    [SerializeField] 
+    private Text Time_Display;
+    
+    
+    // Bool
+    public bool isPaused;
+    public bool FreePlay;
+    
+    CursorLockMode wantedMode;
+    
+    
+
+    void Start()
+    {
+        wantedMode = CursorLockMode.Locked;
+        isPaused = false;
+        score = 0;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = true;
+        }
+
+        if (isPaused)
+        {
+            Paused();
+        }
+         if(isPaused == false)
+        {
+            wantedMode = CursorLockMode.Locked;
+            if (FreePlay == false)
+            {
+                Time_left -= Time.deltaTime;
+                if (Time_left < 0)
+                {
+                    min -= 1;
+                    Time_left = 60;
+                    if (min <= 0)
+                    {
+                        GameOver();
+                    }
+                }
+            }
+        }
+
+
+         int sec = Mathf.RoundToInt(Time_left);
+
+         Time_Display.text = "Time: " + min + ":" + sec;
+         Score_Display.text = score.ToString();
+        SetCursorState();
+    }
+
+
+    void Paused()
+    {
+        wantedMode = CursorLockMode.None;
+    }
+    void SetCursorState()
+    {
+        Cursor.lockState = wantedMode;
+        Cursor.visible = (CursorLockMode.Locked != wantedMode);
+    }
+
+    void GameOver()
+    {
+        
+    }
+}
--- Assets/Scripts/player_controller.cs
+++ Assets/Scripts/player_controller.cs
@@ -10,6 +10,9 @@ public class player_controller : MonoBehaviour
     private player_motor PM;
     private CapsuleCollider Collider;
     private Rigidbody RB;
+    [SerializeField]
+    private Game_manager GM;
+    
 
     // Data Holders
     [SerializeField]
@@ -20,8 +23,6 @@ public class player_controller : MonoBehaviour
     private bool IsGrounded;
     
 
-
-    // Start is called before the first frame update
     void Start()
     {
         PM = GetComponent<player_motor>();
@@ -31,42 +32,52 @@ public class player_controller : MonoBehaviour
 
 
 
-    // Update is called once per frame
     void Update()
     {
-        // Calculate movement velocities
-        float _Xmove = Input.GetAxisRaw("Horizontal");
-        float _Ymove = Input.GetAxisRaw("Vertical");
-
-        Vector3 _movHorizontal = transform.right * _Xmove;
-        Vector3 _movVertical = transform.forward * _Ymove;
-
-        // Final Move Vector
-        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
-
-        // Apply Movement
-        PM.Move(_velocity);
-
-        // Calculate rotation vectors
-        float _Yrot = Input.GetAxisRaw("Mouse X");
-
-        Vector3 _rotation = new Vector3(0f, _Yrot, 0f) * lookSensitivity;
-
-        PM.Rotate(_rotation);
-
-        // Calculate Camera Rotations
-        float _Xrot = Input.GetAxisRaw("Mouse Y");
-
-        float _cameraRotationX = _Xrot * lookSensitivity;
-
-        //apply Camera Rotation
-        PM.RotateCamera(_cameraRotationX);
-
-        // Jump
-        if (Input.GetButtonDown("Jump") && IsGrounded)
+        if (!GM.isPaused)
         {
-            RB.velocity = new Vector2(RB.velocity.x, jumpSpeed);
-            IsGrounded = false;
+            // Calculate movement velocities
+            float _Xmove = Input.GetAxisRaw("Horizontal");
+            float _Ymove = Input.GetAxisRaw("Vertical");
+
+            Vector3 _movHorizontal = transform.right * _Xmove;
+            Vector3 _movVertical = transform.forward * _Ymove;
+
+            // Final Move Vector
+            Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+            
+            
+            // Apply Movement
+            PM.Move(_velocity);
+
+            // Calculate rotation vectors
+            float _Yrot = Input.GetAxisRaw("Mouse X");
+
+            Vector3 _rotation = new Vector3(0f, _Yrot, 0f) * lookSensitivity;
+
+            PM.Rotate(_rotation);
+
+            // Calculate Camera Rotations
+            float _Xrot = Input.GetAxisRaw("Mouse Y");
+
+            float _cameraRotationX = _Xrot * lookSensitivity;
+
+            //apply Camera Rotation
+            PM.RotateCamera(_cameraRotationX);
+
+            // Jump
+            if (Input.GetButtonDown("Jump") && IsGrounded)
+            {
+                RB.velocity = new Vector2(RB.velocity.x, jumpSpeed);
+                IsGrounded = false;
+            }
+        }else if (GM.isPaused)
+        {
+            PM.Move(Vector3.zero);
+            PM.Rotate(Vector3.zero);
+            PM.RotateCamera(0f);
+
         }
     }
 
